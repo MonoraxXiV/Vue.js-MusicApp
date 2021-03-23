14d38249a16add0ec182132e10086dcc86774380
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2021-03-23 10:06:57 +0100

    Sound now plays, progress!

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
+  <!--html code-->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>My music</h1>
+    </header>
+    <main>
+      <section class="player">
+        <h2 class="songTitle">{{ current.title }} - <span>{{current.artist}}</span></h2>
+        <!--placing controls here-->
+        <div class="controls">
+          <button>Previous</button>
+          <button>Play</button>
+          <button>Pause</button>
+          <button>Next</button>
+        </div>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+/*Javascript code*/
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  /*  data works similar to state in react in this case*/
+  data() {
+    return {
+      current: {
+  /*      title: 'SONG TITLE'*/
+      },
+      index: 1,
+      songs: [
+        {
+          /*Example song obtained from Bensound: https://www.bensound.com/royalty-free-music/track/enigmatic*/
+        title: 'Enigmatic',
+          artist: 'Bensound',
+          src: require('./assets/bensound-enigmatic.mp3')
+        },
+        {
+         /* Example song obtained from Bensound: https://www.bensound.com/royalty-free-music/track/epic*/
+          title: 'Epic',
+          artist: 'Bensound',
+          src: require('./assets/bensound-epic.mp3')
+        },
+        {
+          /* Example song obtained from Bensound: https://www.bensound.com/royalty-free-music/track/evolution*/
+          title: 'Epic',
+          artist: 'Bensound',
+          src: require('./assets/bensound-evolution.mp3')
+        }
+      ],
+      player: new Audio(),
+    }
+  },
+  created (){
+      this.current = this.songs[this.index];
+      this.player.src = this.current.src;
+     /* comment/uncomment to test wether it plays*/
+      this.player.play();
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/*css code*/
+
+* {
+  /*removes browser default settings*/
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  background-color: darkslateblue;
+  color: white;
+}
+h2{
+  padding: 3px;
 }
 </style>
