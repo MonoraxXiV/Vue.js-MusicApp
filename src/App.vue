3a966d136347464d931f81c77a8a7ca2ee9d47be
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2021-03-23 15:51:26 +0100

    fixed the issue with next,previous added as well, looking into what to do when the song ends and styling

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,17 @@
         <h2 class="songTitle">{{ current.title }} - <span>{{current.artist}}</span></h2>
         <!--placing controls here-->
         <div class="controls">
-          <button class="previous">Previous</button>
+          <button class="previous" @click="Previous">Previous</button>
           <button class="play" v-if="!isPlaying" @click="play">Play</button>
           <button class="pause" v-else @click="Pause">Pause</button>
-          <button class="next">Next</button>
+          <button class="next" @click="Next">Next</button>
         </div>
+      </section>
+      <section class="playlist">
+        <h3>Playlist</h3>
+        <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src === current.src)? 'song playing' : 'song'">
+       {{song.number}} {{song.title}} - {{song.artist}}
+        </button>
       </section>
     </main>
   </div>
@@ -27,26 +33,29 @@ export default {
   data() {
     return {
       current: {
-  /*      title: 'SONG TITLE'*/
+
       },
       isPlaying: false,
-      index: 1,
+      index: 0,
       songs: [
         {
           /*Example song obtained from Bensound: https://www.bensound.com/royalty-free-music/track/enigmatic*/
+          number:'1',
         title: 'Enigmatic',
           artist: 'Bensound',
           src: require('./assets/bensound-enigmatic.mp3')
         },
         {
          /* Example song obtained from Bensound: https://www.bensound.com/royalty-free-music/track/epic*/
+          number:'2',
           title: 'Epic',
           artist: 'Bensound',
           src: require('./assets/bensound-epic.mp3')
         },
         {
           /* Example song obtained from Bensound: https://www.bensound.com/royalty-free-music/track/evolution*/
-          title: 'Epic',
+          number:'3',
+          title: 'Evolution',
           artist: 'Bensound',
           src: require('./assets/bensound-evolution.mp3')
         }
@@ -66,6 +75,24 @@ export default {
     Pause(){
       this.player.pause();
       this.isPlaying=false;
+    },
+    Next(){
+      this.index++;
+      if (this.index > this.songs.length-1){
+        this.index=0;
+      }
+      /*here we change the index so another song will play, you forgot this earlier which is why next wouldn't work'*/
+      this.current=this.songs[this.index];
+      this.play(this.current)
+    },
+    Previous(){
+      this.index--;
+      /*if less than 0, bring back to the end of the list*/
+      if (this.index < 0){
+          this.index= this.songs.length-1;
+      }
+      this.current=this.songs[this.index];
+      this.play(this.current)
     },
   },
   created (){
