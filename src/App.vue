7b3f11d59ba43fb319e88327af16ef945e01cea8
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2021-03-24 11:54:52 +0100

    imported font, testing some lay out, trying to get a background image for final change

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,15 @@
     <main>
       <section class="player">
         <h2 class="songTitle">{{ current.title }} - <span>{{current.artist}}</span></h2>
+      </section>
         <!--placing controls here-->
         <div class="controls">
-          <button class="previous" @click="Previous">Previous</button>
-          <button class="play" v-if="!isPlaying" @click="play">Play</button>
-          <button class="pause" v-else @click="Pause">Pause</button>
-          <button class="next" @click="Next">Next</button>
+          <button class="previous" @click="Previous"> ◄ Previous</button>
+          <button class="play" v-if="!isPlaying" @click="play"> Play</button>
+          <button class="pause" v-else @click="Pause">  Pause</button>
+          <button class="next" @click="Next">Next ► </button>
         </div>
-      </section>
+
       <section class="playlist">
         <h3>Playlist</h3>
         <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src === current.src)? 'song playing' : 'song'">
@@ -58,7 +59,7 @@ export default {
           title: 'Evolution',
           artist: 'Bensound',
           src: require('./assets/bensound-evolution.mp3')
-        }
+        },
       ],
       player: new Audio(),
     }
@@ -70,6 +71,17 @@ export default {
         this.player.src = this.current.src;
       }
       this.player.play();
+
+      this.player.addEventListener('ended',function(){
+        this.index++;
+        if (this.index > this.songs.length-1){
+          this.index=0;
+        }
+        /*here we change the index so another song will play, you forgot this earlier which is why next wouldn't work'*/
+        this.current=this.songs[this.index];
+        this.play(this.current)
+
+      }.bind(this))
       this.isPlaying=true;
     },
     Pause(){
@@ -107,22 +119,61 @@ export default {
 
 <style>
 /*css code*/
-
+/*attempt to import font*/
+@import url('https://fonts.googleapis.com/css2?family=DotGothic16&display=swap');
 * {
   /*removes browser default settings*/
   margin: 0;
   padding: 0;
 }
-
+body{
+  background-color: #554c7b;
+  color: white;
+  background-image: image("https://cdn.discordapp.com/attachments/792473441390428170/823688284742877184/ffxiv_22032021_234231_957.png");
+}
+.controls{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 5px;
+}
+.player{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 header {
+  font-family: 'DotGothic16', sans-serif;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 20px;
+  margin-bottom: 5px;
   background-color: darkslateblue;
   color: white;
 }
 h2{
+  padding: 3px;
+  font-family: 'DotGothic16', sans-serif;
+}
+h3{
+  font-family: 'DotGothic16', sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top:10px;
+  padding:5px;
+}
+
+.playlist{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+}
+
+button{
+  margin: 3px;
   padding: 3px;
 }
 </style>
